b9f3587921f5348aaa74bdbd97733c73455e3d7b
diegoachury <tiran_o@hotmail.com>
2013-02-15 18:54:53 -0500

    inicio el repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lubuinv::Application.config.secret_token = '8ad4a6171ba11f63a1550f143fa07274f2b5500791883255a8ff503224ce4ba7dc10e6c6f48af0ad6323d30beb5bdecb1e623b3ea14728e577c8efc32b091903'
